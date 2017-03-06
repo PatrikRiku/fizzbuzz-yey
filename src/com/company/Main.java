2e6a273feaee3c2ef7c12769fca3d53fe5d3e56a
theGurk <gustav.e.bergsten@gmail.com>
2017-03-06 10:01:55 +0100

    My Fizzbuzz solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String buzz =  "buzz", fizz = "fizz"; //initialise the string variables
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 15 == 0) //check if number in position i is divisable by 15, if so don't check other 2 conditions - we don't want a double print
+            {
+                System.out.println(buzz + fizz + " " + i);
+            }
+            else if (i % 3  == 0 )
+            {
+                System.out.println(buzz + " " + i);
+            }
+            else if (i % 5== 0)
+            {
+                System.out.println(fizz + " "+ i);
+            }
+        }
     }
 }
