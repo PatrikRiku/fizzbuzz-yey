d09814a1c66f35a59bfc9870f681836370442cb5
theGurk <gustav.e.bergsten@gmail.com>
2017-03-06 10:57:06 +0100

    My Fizzbuzz solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String buzz =  "buzz", fizz = "fizz"; //initialise the string variables
+        String buzz =  "buzz", fizz = "fizz"; //initialise the string variables RIKU
         for (int i = 1; i <= 100; i++)
         {
             if (i % 15 == 0) //check if number in position i is divisable by 15, if so don't check other 2 conditions - we don't want a double print
